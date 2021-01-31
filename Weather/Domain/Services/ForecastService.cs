2346da514cab0c8e8b521594b6aafcb5496f69de
AndreyZabuyskiy <57301364+AndreyZabuyskiy@users.noreply.github.com>
2021-01-31 13:42:20 +0200

    Create forecast service

--- Weather/Domain/Services/ForecastService.cs
+++ Weather/Domain/Services/ForecastService.cs
@@ -2,10 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Weather.Domain.Models;
+using Weather.Domain.UseCases;
 
 namespace Weather.Domain.Services
 {
-    public class ForecastService
+    public class ForecastService : IGetCurrentWeather
     {
+        private readonly IRequestCurrentWeather _requestCurrentWeather;
+        private readonly IRequestForecast _requestForecast;
+
+        public ForecastService(IRequestCurrentWeather requestCurrentWeather)
+        {
+            _requestCurrentWeather = requestCurrentWeather;
+        }
+
+        public async Task<WeatherData> GetCurrentWeather(string city)
+        {
+            dynamic data = await _requestCurrentWeather.RequestCurrentWeather(city);
+            Temperature temp = new Temperature(data.main.temp, data.main.temp_min, data.main.temp_max, data.main.feels_like);
+            Wind wind = new Wind(data.wind.speed, data.wind.deg);
+            Sky sky = new Sky(data.weather[0].description, data.clouds.all);
+            WeatherData weather = new WeatherData(temp, wind, sky);
+
+            return weather;
+        }
     }
 }
--- Weather/Domain/UseCases/IGetCurrentWeather.cs
+++ Weather/Domain/UseCases/IGetCurrentWeather.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Weather.Domain.Models;
 
 namespace Weather.Domain.UseCases
 {
-    interface IGetCurrentWeather
+    public interface IGetCurrentWeather
     {
-        void GetCurrentWeather(string city);
+        Task<WeatherData> GetCurrentWeather(string city);
     }
 }
